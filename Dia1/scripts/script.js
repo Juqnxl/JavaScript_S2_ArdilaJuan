38736565655a08c124542c7ced37eea871195d2a
Juqnxl <juanestebana07@gmail.com>
2025-07-18 13:08:07 -0500

    feat: :sparkles: introduction to JavaScript

---
+++ Dia1/scripts/script.js
@@ -0,0 +1,36 @@
+// ###################
+// ###### DIA 1 ######
+// ###################
+
+// Primer comando!
+
+console.log("Hola mundo!");
+// ###################
+// ###### DATA PRIMITIVA ######
+// ###################
+//nUMBER
+variable1= 1.14;
+console.log(variable1);
+console.log(typeof variable1);
+
+//Booleanos
+booleanito1=true;
+booleanito2=false;
+console.log(typeof booleanito1)
+//Condicionales
+if (booleanito1 == true){
+    console.log("Es verdadero!!");
+}else{
+    console.log("No es verdadero :sadfeis:");
+}
+//Bucles
+for(i=0;i<4;i=i+1){
+    console.log("Hableloo");
+    
+}
+hola = true;
+while(hola==true){
+    console.log("hablalo");
+    hola=false;
+}
+// Desarrollado por : Juan ardila - C.C:1.097.496.297
