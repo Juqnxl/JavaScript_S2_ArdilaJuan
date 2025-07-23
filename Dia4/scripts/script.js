2fdaef03088e3687cf4832c4e61402eb110011a2
Juqnxl <juanestebana07@gmail.com>
2025-07-23 13:01:22 -0500

    feat: ✨ quiz

---
+++ Dia4/scripts/script.js
@@ -0,0 +1,17 @@
+// ###################
+// ###### DIA 4 ######
+// ###################
+
+// Hamburguesas
+funcionPrograma = true
+
+while(funcionPrograma);
+opcionUsuario = prompt("=============================================\n              Menu Principal                     \n=============================================\nSeleccione una opcion:\n \n1) Registrar nuevo gasto\n2) Lista de gastos\n3) Actualizar gasto\n4) Elminar gasto\n=============================================");
+if (opcionUsuario ==1);
+prompt8("hola");
+
+
+
+
+
+// Desarrollado por : Juan ardila - C.C:1.097.496.297
